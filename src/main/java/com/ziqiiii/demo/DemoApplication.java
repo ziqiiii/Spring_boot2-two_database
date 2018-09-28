ac515bd01d560c3d1701c96b5db26aa3f67e844c
ziqiiii <522243591@qq.com>
2018-09-28 14:44:26 +0800

    add file

---
+++ src/main/java/com/ziqiiii/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.ziqiiii.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * create by ziqi.zhang on 2018/9/28
+ */
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+    }
+}
